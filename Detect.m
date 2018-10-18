a870e813c08d2f219c05752e382724776b34e8ae
sahilMakandar <sahilmakandar2@gmail.com>
2018-10-18 09:32:14 +0530

    Update Detect.m

--- Detect.m
+++ Detect.m
@@ -30,57 +30,15 @@ I_HIS = rgb2hsi(I);
 cform = makecform('srgb2lab');
 % Apply the colorform
 lab_he = applycform(I,cform);
-%G = imoverlay(zero_image,seg_img,[0 1 0]);
-%G = imoverlay(zero_image,seg_img,[1 0 0]);
-
-%cc = bwconncomp(seg_img,6);
-%diseasedata = regionprops(cc,'basic');
-%A1 = diseasedata.Area;
-%sprintf('Area of the disease affected region is : %g%',A1);
-%cc = bwconncomp(seg_img,5);
-%G = imoverlay(zero_image,seg_img,[1 0 0]);
-
-%cc = bwconncomp(seg_img,6);
-%diseasedata = regionprops(cc,'basic');
-%A1 = diseasedata.Area;
-%sprintf('Area of the disease affected region is : %g%',A1);
-%diseasedata = regionprops(cc,'basic');
-%A1 = diseasedata.Area;
-%sprintf('Area of the disease affected region is : %g%',A1);
-% Classify the colors in a*b* colorspace using K means clustering.
-% Since the image has 3 colors create 3 clusters.
-% Measure the distance using Euclidean Distance Metric.
 ab = double(lab_he(:,:,2:3));
 nrows = size(ab,1);
 ncols = size(ab,2);
 ab = reshape(ab,nrows*ncols,2);
 nColors = 3;
-%ab = double(lab_he(:,:,1:3));
-%nrows = size(ab,1);
-%ncols = size(ab,2);
-%ab = reshape(ab,nrows*ncols,3);
-%nColors = 3;
-%[cluster_idx cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean', ...
-                                 
-%[cluster_idx cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean','Replicates',3);
-% Label every pixel in tha image using results from K means
-%pixel_labels = reshape(cluster_idx,nrows,ncols);
-%figure,imshow(pixel_labels,[]), title('Image Labeled by Cluster Index');
+
 [cluster_idx cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean', ...
                                       'Replicates',3);
-%[cluster_idx cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean','Replicates',3);
-% Label every pixel in tha image using results from K means
-%G = imoverlay(zero_image,seg_img,[1 0 0]);
-%G = imoverlay(zero_image,seg_img,[1 0 0]);
 
-%cc = bwconncomp(seg_img,6);
-%diseasedata = regionprops(cc,'basic');
-%A1 = diseasedata.Area;
-%sprintf('Area of the disease affected region is : %g%',A1);
-%cc = bwconncomp(seg_img,6);
-%diseasedata = regionprops(cc,'basic');
-%A1 = diseasedata.Area;
-%sprintf('Area of the disease affected region is : %g%',A1);
 pixel_labels = reshape(cluster_idx,nrows,ncols);
 %figure,imshow(pixel_labels,[]), title('Image Labeled by Cluster Index');
 
@@ -95,27 +53,7 @@ for k = 1:nColors
     segmented_images{k} = colors;
 end
 
-%ab = double(lab_he(:,:,1:3));
-%nrows = size(ab,1);
-%ncols = size(ab,2);
-%ab = reshape(ab,nrows*ncols,2);
-%nColors = 3;
-%[cluster_idx cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean', ...
-      %ab = double(lab_he(:,:,2:3));
-%nrows = size(ab,1);
-%ncols = size(ab,2);
-%ab = reshape(ab,nrows*ncols,2);
-%nColors = 3;
-%[cluster_idx cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean', ...
-                                     
-%[cluster_idx cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean','Replicates',3);
-% Label every pixel in tha image using results from K means
-%pixel_labels = reshape(cluster_idx,nrows,ncols);
-%figure,imshow(pixel_labels,[]), title('Image Labeled by Cluster Index');                                'Replicates',3);
-%[cluster_idx cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean','Replicates',3);
-% Label every pixel in tha image using results from K means
-%pixel_labels = reshape(cluster_idx,nrows,ncols);
-%figure,imshow(pixel_labels,[]), title('Image Labeled by Cluster Index');
+
 
 figure, subplot(3,1,1);imshow(segmented_images{1});title('Cluster 1'); subplot(3,1,2);imshow(segmented_images{2});title('Cluster 2');
 subplot(3,1,3);imshow(segmented_images{3});title('Cluster 3');
@@ -124,18 +62,7 @@ subplot(3,1,3);imshow(segmented_images{3});title('Cluster 3');
 % Feature Extraction
 x = inputdlg('Enter the cluster no. containing the ROI only:');
 i = str2double(x);
-%ab = double(lab_he(:,:,2:3));
-%nrows = size(ab,1);
-%ncols = size(ab,2);
-%ab = reshape(ab,nrows*ncols,2);
-%nColors = 3;
-%[cluster_idx cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean', ...
-                                 
-%[cluster_idx cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean','Replicates',3);
-% Label every pixel in tha image using results from K means
-%pixel_labels = reshape(cluster_idx,nrows,ncols);
-%figure,imshow(pixel_labels,[]), title('Image Labeled by Cluster Index');
-% Extract the features from the segmented image
+
 seg_img = segmented_images{i};
 
 % Convert to grayscale if image is RGB
@@ -151,117 +78,30 @@ m = size(seg_img,1);
 n = size(seg_img,2);
 
 zero_image = zeros(m,n); 
-%G = imoverlay(zero_image,seg_img,[1 0 0]);
-%G = imoverlay(zero_image,seg_img,[1 0 0]);
 
-%cc = bwconncomp(seg_img,6);
-%diseasedata = regionprops(cc,'basic');
-%A1 = diseasedata.Area;
-%sprintf('Area of the disease affected region is : %g%',A1);
-
-%I_black = im2bw(I,graythresh(I));
-%kk = bwconncomp(I,6);
-%leafdata = regionprops(kk,'basic');
-%A2 = leafdata.Area;
-%sprintf(' Total leaf area is : %g%',A2);
-
-%Affected_Area = 1-(A1/A2);
-%Affected_Area = (A1/A2);
-%if Affected_Area < 1
-   % Affected_Area = Affected_Area+0.15;
-%end
 cc = bwconncomp(seg_img,6);
 diseasedata = regionprops(cc,'basic');
 A1 = diseasedata.Area;
 sprintf('Area of the disease affected region is : %g%',A1);
-%G = imoverlay(zero_image,seg_img,[1 0 0]);
 
-%cc = bwconncomp(seg_img,4);
-%diseasedata = regionprops(cc,'basic');
-%A1 = diseasedata.Area;
-%sprintf('Area of the disease affected region is : %g%',A1);
-
-%I_black = im2bw(I,graythresh(I));
-%kk = bwconncomp(I,6);
-%leafdata = regionprops(kk,'basic');
-%A2 = leafdata.Area;
-%sprintf(' Total leaf area is : %g%',A2);
-
-%Affected_Area = 1-(A1/A2);
-%Affected_Area = (A1/A2);
-%if Affected_Area < 1
-   % Affected_Area = Affected_Area+0.15;
-%end
 I_black = im2bw(I,graythresh(I));
 kk = bwconncomp(I,6);
 leafdata = regionprops(kk,'basic');
 A2 = leafdata.Area;
 sprintf(' Total leaf area is : %g%',A2);
-%G = imoverlay(zero_image,seg_img,[1 0 0]);
 
-%cc = bwconncomp(seg_img,2);
-%diseasedata = regionprops(cc,'basic');
-%A1 = diseasedata.Area;
-%sprintf('Area of the disease affected region is : %g%',A1);
-
-%I_black = im2bw(I,graythresh(I));
-%kk = bwconncomp(I,6);
-%leafdata = regionprops(kk,'basic');
-%A2 = leafdata.Area;
-%sprintf(' Total leaf area is : %g%',A2);
-
-%Affected_Area = 1-(A1/A2);
-%Affected_Area = (A1/A2);
-%if Affected_Area < 1
-   % Affected_Area = Affected_Area+0.15;
-%end
-%Affected_Area = 1-(A1/A2);
 Affected_Area = (A1/A2);
 if Affected_Area < 1
     Affected_Area = Affected_Area+0.15;
 end
 sprintf('Affected Area is: %g%%',(Affected_Area*100))
-%G = imoverlay(zero_image,seg_img,[1 0 0]);
 
-%cc = bwconncomp(seg_img,6);
-%diseasedata = regionprops(cc,'basic');
-%A1 = diseasedata.Area;
-%sprintf('Area of the disease affected region is : %g%',A1);
-
-%I_black = im2bw(I,graythresh(I));
-%kk = bwconncomp(I,6);
-%leafdata = regionprops(kk,'basic');
-%A2 = leafdata.Area;
-%sprintf(' Total leaf area is : %g%',A2);
-
-%Affected_Area = 1-(A1/A2);
-%Affected_Area = (A1/A2);
-%if Affected_Area < 1
-   % Affected_Area = Affected_Area+0.15;
-%end
-% Create the Gray Level Cooccurance Matrices (GLCMs)
 glcms = graycomatrix(img);
 
-% Derive Statistics from GLCM
-%stats = graycoprops(glcms,'Contrast Correlation Energy Homogeneity');
-%Contrast = stats.Contrast;
-%Correlation = stats.Correlation;
-%Energy = stats.Energy;
-%Homogeneity = stats.Homogeneity;
-%Mean = mean2(seg_img);
+
 stats = graycoprops(glcms,'Contrast Correlation Energy Homogeneity');
 Contrast = stats.Contrast;
 Correlation = stats.Correlation;
-%Skewness = skewness(img)
-%Variance = mean2(var(double(seg_img)));
-%a = sum(double(seg_img(:)));
-%Smoothness = 1-(1/(1+a));
-%Kurtosis = kurtosis(double(seg_img(:)));
-%Skewness = skewness(double(seg_img(:)));
-% Inverse Difference Movement
-%m = size(seg_img,1);
-%n = size(seg_img,2);
-%in_diff = 0;
 Energy = stats.Energy;
 Homogeneity = stats.Homogeneity;
 Mean = mean2(seg_img);
@@ -278,12 +118,7 @@ Skewness = skewness(double(seg_img(:)));
 m = size(seg_img,1);
 n = size(seg_img,2);
 in_diff = 0;
-%for i = 1:n
-   % for j = 1:m
-        %temp = seg_img(i,j)./(1+(i-j).^2);
-        %in_diff = in_diff+temp;
-   % end
-%end
+
 for i = 1:m
     for j = 1:n
         temp = seg_img(i,j)./(1+(i-j).^2);
@@ -323,18 +158,7 @@ end
 %% Evaluate Accuracy
 load('Accuracy_Data.mat')
 Accuracy_Percent= zeros(200,1);
-%for i = 1:500
-%data = Train_Feat;
-%groups = ismember(Train_Label,1);
-%[train,test] = crossvalind('HoldOut',groups);
-%cp = classperf(groups);
-%svmStruct = svmtrain(data(train,:),groups(train),'showplot',false,'kernel_function','linear');
-%classes = svmclassify(svmStruct,data(test,:),'showplot',false);
-%classperf(cp,classes,test);
-%Accuracy = cp.CorrectRate;
-%Accuracy_Percent(i) = Accuracy.*100;
-%end
-%Max_Accuracy = max(Accuracy_Percent);
+
 for i = 1:500
 data = Train_Feat;
 %groups = ismember(Train_Label,1);
@@ -342,13 +166,7 @@ groups = ismember(Train_Label,0);
 [train,test] = crossvalind('HoldOut',groups);
 cp = classperf(groups);
 
-%svmStruct = svmtrain(data(train,:),groups(train),'showplot',false,'kernel_function','linear');
-%classes = svmclassify(svmStruct,data(test,:),'showplot',false);
-%classperf(cp,classes,test);
-%Accuracy = cp.CorrectRate;
-%Accuracy_Percent(i) = Accuracy.*100;
-%end
-%Max_Accuracy = max(Accuracy_Percent);
+
 svmStruct = svmtrain(data(train,:),groups(train),'showplot',false,'kernel_function','linear');
 classes = svmclassify(svmStruct,data(test,:),'showplot',false);
 classperf(cp,classes,test);
